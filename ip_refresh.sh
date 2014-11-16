758001bcabbdaba5bc3ae8dc4762de55586ef31f
Vito Mule <mulevito@gmail.com>
2014-11-16 19:50:16 +0000

    comment

--- ip_refresh.sh
+++ ip_refresh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # IP refresher for hosts in /etc/hosts
+# This will match a line like this: '192.168.1.1 home' and update the IP address
+# with the IP in the file /Users/$WHOAMI/Google\ Drive/.myip
 
 WHOAMI=`whoami`
 FRESH_IP=`cat /Users/$WHOAMI/Google\ Drive/.myip`
